c0d7d4916903305c6fe94ff0bf138c0872069c0c
Hans Heje <hansheje@hotmail.com>
2022-11-08 21:35:27 +0100

    version 1 done

--- src/pages/publications/PublicationsPage.js
+++ src/pages/publications/PublicationsPage.js
@@ -1,9 +1,16 @@
 import '../../App.css';
+import Article from './modules/Article'
 
 function PublicationsPage() {
     return (
         <div>
+            <h1>2023</h1>
 
+            <Article title="Low Resource Languages: Haitian" authors="Johannes Bjerva, Heather Christine Lint, Hans Heje" journal="Computational linguistics" paper="https://arxiv.org/abs/2004.14283" text="placeholder text blah blah" />
+
+            <h1>2022</h1>
+            <Article title="Low Resource Languages: Haitian" authors="Johannes Bjerva, Heather Christine Lint, Hans Heje" journal="Computational linguistics" paper="https://arxiv.org/abs/2004.14283" text="placeholder text blah blah" />
+            <Article title="Low Resource Languages: Haitian" authors="Johannes Bjerva, Heather Christine Lint, Hans Heje" journal="Computational linguistics" paper="https://arxiv.org/abs/2004.14283" text="placeholder text blah blah" />
         </div>
     );
 }
